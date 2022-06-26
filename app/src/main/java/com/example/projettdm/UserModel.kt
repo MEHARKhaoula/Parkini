eac3233f3149b49b786e0dd2daa7ef0cdb99d4fb
manelmech <im_mechouar@esi.dz>
2022-06-26 17:20:11 +0100

    New version

--- app/src/main/java/com/example/projettdm/UserModel.kt
+++ app/src/main/java/com/example/projettdm/UserModel.kt
@@ -1,11 +1,16 @@
 package com.example.projettdm
 
+import java.util.*
+
 data class UserModel(
     val iduser:Int,
     val nom:String,
     val prenom :String,
     val numero_telephone:String,
     val email:String,
-    val mot_de_passse:String,
+    val mot_de_passe:String,
+
+
+
 )
 
